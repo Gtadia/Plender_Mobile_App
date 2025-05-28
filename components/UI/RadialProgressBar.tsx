14f8e55361b7ccc1520482d96a7ddbb952632e97
Min Han <m235humit9@gmail.com>
2025-05-28 14:41:50 -0400

    basic radial progress bar finished

--- components/UI/RadialProgressBar.tsx
+++ components/UI/RadialProgressBar.tsx
@@ -1,18 +1,29 @@
 import { colorTheme$ } from "@/utils/stateManager";
 import { observer } from "@legendapp/state/react";
 import { Canvas, Path, Skia } from "@shopify/react-native-skia";
-import { View } from "react-native";
+import { Dimensions, View, Text } from "react-native";
 
 // var { width, height } = Dimensions.get("window");
 
-const RADIUS = 305 / 2;
-const DIAMETER = 2 * RADIUS;
+// TODO — Have a max width of like 500px
+// TODO — Diameter = Dimension.
+
+const MAX_DIAMETER = 800; // TODO — find if this looks good
 
 const RadialProgressBar = observer(
   ({ time_goal, time_spent, currentHandler, item }: any) => {
+    const windowWidth = Dimensions.get('window').width;
+
+    const strokeWidth = 30;
+    const strokeWidth2 = strokeWidth / 2;
+
+    const DIAMETER = (windowWidth - 40 - (strokeWidth + strokeWidth2)/2) > MAX_DIAMETER ? MAX_DIAMETER : (windowWidth - 40 - (strokeWidth + strokeWidth2)/2);
+    const RADIUS = DIAMETER / 2;
+
     const color = "#e68f40"; // TODO — gradient change based on percentage completed (color-interpolate --> reanimated)
     const radius = RADIUS;
-    const strokeWidth = 30;
+
+
 
     const innerRadius = radius - strokeWidth / 2;
     const outerDiameter = radius * 2;
@@ -22,7 +33,6 @@ const RadialProgressBar = observer(
     path.addCircle(radius + outerBuffer, radius + outerBuffer, innerRadius);
 
 
-    const strokeWidth2 = strokeWidth / 2;
     const radius2 = (RADIUS + (strokeWidth) / 2);
 
     const innerRadius2 = radius2 - strokeWidth2 / 2;
@@ -33,7 +43,10 @@ const RadialProgressBar = observer(
     outerPath.addCircle(radius2 + outerBuffer2, radius2 + outerBuffer2, innerRadius2)
 
     return (
-      <View style={{justifyContent: "center", alignItems: "center", paddingTop: strokeWidth2}}>
+      <View style={{justifyContent: "flex-start", alignItems: "center", marginTop: strokeWidth2,             borderWidth: 5,
+            borderColor: 'red', height: outerDiameter2 + strokeWidth2 - 5}}>
+              {/* TODO — IDK why I have to subtract by 5... */}
+{/* General Task Progress Radius */}
         <View
           style={{
             width: outerDiameter + strokeWidth,
@@ -65,6 +78,7 @@ const RadialProgressBar = observer(
           </Canvas>
         </View>
 
+{/* Current Task Progress Radius */}
         <View
           style={{
             width: outerDiameter2 + strokeWidth2,
@@ -96,6 +110,37 @@ const RadialProgressBar = observer(
               end={0.7}
             />
           </Canvas>
+        </View>
+
+{/* Info Text */}
+        <View
+          style={{
+            width: outerDiameter2 + strokeWidth2,
+            height: outerDiameter2 + strokeWidth2,
+            top: -1 * ((outerDiameter2 + strokeWidth2) + (outerDiameter2 + strokeWidth2/2)),
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <View
+            style={{
+              height: 215,
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          >
+            <Text>
+              {"5%"}
+            </Text>
+
+            <Text>
+              {"3:15:42"}
+            </Text>
+
+            <Text>
+              {"4:00:00"}
+            </Text>
+          </View>
         </View>
       </View>
     );
